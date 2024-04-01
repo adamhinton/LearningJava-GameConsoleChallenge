e11e2dcb33f89497a99d0e3fb9cc62c109e2ea21
adamd <adam.d.hinton@gmail.com>
2024-04-01 00:08:39 -0400

    Init commit: Done w setup; bout to start on actual challenge

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
 
 
+
         var console = new GameConsole<>(new ShooterGame("The Shootout Game"));
 
         int playerIndex = console.addPlayer();

dcb51fdc0035b462ea992240116a6ff6adbc269e
adamd <adam.d.hinton@gmail.com>
2024-03-31 23:45:05 -0400

    GameConsole.java

--- src/dev/lpa/game/GameConsole.java
+++ src/dev/lpa/game/GameConsole.java
@@ -21,4 +21,29 @@ public class GameConsole <T extends Game<? extends Player>>{
     }
 
 
+    public void playGame (int playerIndex){
+        boolean done = false;
+        while (!done){
+            var gameActions = game.getGameActions(playerIndex);
+
+            System.out.println("Select from one of the following Actions:");
+
+            for (Character c : gameActions.keySet()){
+                String prompt = gameActions.get(c).prompt();
+                System.out.println("\t" + prompt + " (" + c  + " )");
+            }
+            System.out.println("Enter Next Action:");
+
+            char nextMove = scanner.nextLine().toUpperCase().charAt(0);
+            GameAction gameAction = gameActions.get(nextMove);
+
+            if (gameAction != null){
+                System.out.println("---------------------");
+                done = game.executeGameAction(playerIndex, gameAction);
+                if(! done ){
+                    System.out.println("------------------");
+                }
+            }
+        }
+    }
 }
